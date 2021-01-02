294f7f1f832d75ed1538dab4c7339cad8687a8bf
micbar <56961832+mbarecki@users.noreply.github.com>
2021-01-02 10:11:42 +0100

    Update get_vm_ip script

--- get_vm_ip.sh
+++ get_vm_ip.sh
@@ -5,10 +5,7 @@
 # Name of VM which IP should be returned (retrieved from user input)
 vm_name=$1
 
-# Retrieve MAC address of VM
-vm_mac=$(virsh domiflist "${vm_name}" | awk 'NR>2 {print $5}' | awk '!/^$/')
-
-# Retrieve IP address from dhcp leases for VM MAC
-vm_ip=$(virsh net-dhcp-leases default --mac "${vm_mac}" | awk 'NR>2 { print $5 }')
+# Retrieve VM IP using arp
+vm_ip=$(virsh domifaddr --source arp "${vm_name}" | awk 'NR>2 { print $4 }')
 
 echo "${vm_ip}"
